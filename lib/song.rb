056f0259bdfc5bdf577b8866246e7db79c03e792
Ryan Brady <ryanbrady369@gmail.com>
2018-11-01 21:47:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   end
 
   def artist_name      #may need a cond. to return nil if no artist, not sure what it returns without right now
-    self.artist.name ? self.artist.name : nil
+    self.artist.name.is_a?(string) ? self.artist.name : nil
   end
 
 end
