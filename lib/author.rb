d9f205289b1fbefa6755e1477ad45071016ee897
Ryan Brady <ryanbrady369@gmail.com>
2018-11-01 22:28:39 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,14 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post = Post.new(post)
+    @posts << post
+  end
+
+end 
